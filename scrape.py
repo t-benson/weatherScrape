c2dbbdfb92fd458251c8d1a642f99574be1ce332
Tim Benson <tim@example.com>
2018-12-26 17:10:06 -0500

    Update scrape.py
    Add database support for mysql

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 # Scrape weather from:
 # https://www.almanac.com/weather/history/MA/Boston/1984-01-14
 
+import mysql.connector
 import time
 from datetime import date
 from datetime import timedelta
@@ -23,15 +24,15 @@ def get_temps(date, city="Boston", state="MA"):  # example: (1982-01-01, Boston,
     req = urllib.request.Request(url, headers=hdr)
     response = urllib.request.urlopen(req)
 
-    response_info = response.info()
+    #response_info = response.info()
 
-    response_code = response.code
-    response_reason = response.reason
-    response_headers = response.headers
+    #response_code = response.code
+    #response_reason = response.reason
+    #response_headers = response.headers
     
-    print(response_code)
-    print(response_reason)
-    print(response_headers)
+    #print(response_code)
+    #print(response_reason)
+    #print(response_headers)
 
     html_page = response.readlines()
     html_page = str(html_page)
@@ -40,7 +41,63 @@ def get_temps(date, city="Boston", state="MA"):  # example: (1982-01-01, Boston,
     max_match = re.search(max_reg, html_page)
     mean_match = re.search(mean_reg, html_page)
 
-    return (date, city, state, min_match.group(1), max_match.group(1), mean_match.group(1))
+    guid = date + '_' + city + '_' + state
+
+    return (guid, date, city, state, min_match.group(1), max_match.group(1), mean_match.group(1))
+
+
+def update_db(_date, city="Boston", state="MA"):
+    
+    cnx = mysql.connector.connect(user='scrape', password='P@ssw0rd123^',
+                              host='192.168.0.25', database='temperatures')
+    
+    cursor = cnx.cursor()
+
+    add_scrape = ("INSERT INTO scrapes "
+               "(first_name, last_name, hire_date, gender, birth_date) "
+               "VALUES (%s, %s, %s, %s, %s)")
+
+    db_values = get_temps(_date, city, state)
+
+    print(db_values)
+
+    s_year = int(db_values[1][0:4])
+    s_month = int(db_values[1][5:7])
+    s_day = int(db_values[1][8:11])
+
+    print(s_year)
+    print(s_month)
+    print(s_day)
+
+    print(type(s_year))
+    print(type(s_month))
+    print(type(s_day))
+
+    scrape_date = date(s_year, s_month, s_day)
+
+    print(scrape_date)
+    
+    data_scrape = {
+      'guid': db_values[0],
+      'date': scrape_date,
+      'city': db_values[2],
+      'state': db_values[3],
+      'low': float(db_values[4]),
+      'high': float(db_values[5]),
+      'mean': float(db_values[6])
+    }
+
+    add_scrape = ("INSERT INTO scrapes "
+                  "(guid, date, city, state, low, high, mean) "
+                  "VALUES (%(guid)s, %(date)s, %(city)s, %(state)s, %(low)s, %(high)s, %(mean)s)")
+
+    cursor.execute(add_scrape, data_scrape)
+
+    cnx.commit()
+
+    cursor.close()
+
+    cnx.close()
 
 
 def scrape_dates(beg_date, end_date):  # ('1982-01-01', '1983-01-31')
@@ -83,4 +140,3 @@ def compare_date(date1, date2, city="Boston", state="MA"):
 # get_temps("1982-01-02", "Boston", "MA")
 
 # compare_date("1984-01-01", "2017-01-01")
-
